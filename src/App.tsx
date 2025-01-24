d91ede59c751639a24a996758b51e0759ee47236
gustavocw <optimumsistemasbrasil@gmail.com>
2025-01-24 18:17:40 -0300

    feat: integrate area fetching and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Toaster } from "components/ui/toaster";
 import { AuthProvider } from "contexts/AuthContext";
 import PageMetadata from "routes/meta/pageMetaData";
 import AppRoutes from "routes/routes";
+import { getArea } from "services/area.services";
 import theme from "utils/theme";
 
 function App() {
-  const queryClient = new QueryClient();
+  const url = window.location.origin;
+  const {data: area, isLoading} = useQuery({
+    queryKey: ['area', url],
+    queryFn: async () => {
+      return getArea(url);
+    }
+  })
+
+  if (isLoading) {
+    return <div>Carregando área de membro...</div>;
+  }
 
   return (
     <>
       <ChakraProvider value={theme}>
-        <QueryClientProvider client={queryClient}>
           <AuthProvider>
-            <PageMetadata title={"Nome da área"} faviconUrl={"/vite.svg"} />
+            <PageMetadata title={area[0]?.title} faviconUrl={area[0]?.icon} />
             <AppRoutes />
             <Toaster />
           </AuthProvider>
-        </QueryClientProvider>
       </ChakraProvider>
     </>
   );
---
+++ src/types/area.d.ts
@@ -0,0 +1,21 @@
+interface Course {
+  _id: string;
+  name: string;
+  description: string;
+  thumbnail: string;
+  state: string;
+  category: string;
+  duration?: string;
+}
+
+interface Area {
+  _id: string;
+  domain: string;
+  color: string;
+  title: string;
+  background: string;
+  icon: string;
+  logo: string;
+  courses: Course[];
+  createdAt: string;
+}
